b9b0eed29f8eef474edc628c25c8e765a66b5dd3
me0w <liuguozhu720@gmail.com>
2019-10-28 21:01:25 +0800

    Completed the remaining exercises.

---
+++ chapter2/toe.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void)
+{
+	int toes, doubleToes, squareToes;
+	toes = 10;
+	doubleToes = toes * 2;
+	squareToes = toes * toes;
+	printf("The value of toes is %d, its double is %d, and its square is %d.", toes, doubleToes, squareToes);
+
+	return 0;
+}
